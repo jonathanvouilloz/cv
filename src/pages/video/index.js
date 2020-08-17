245574396c8a643fc1c8404a6a58ee9e51a179e5
Jonathan <jonathanvouilloz@gmail.com>
2020-08-17 10:18:05 +0200

    restest

--- src/pages/video/index.js
+++ src/pages/video/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import {
-  Header,
-  Summary,
-  Experience,
-  Projects,
-  Skills,
-  List,
-  Education,
-  Footer,
-  SEO
-} from '../components';
 
 
 const Home = () => (
   <main>
-    <SEO title="Resume" />
     <div>
-            <iframe src="https://drive.google.com/file/d/1FkzW2UXELpYt0lIu1nTeAx1bKJqQSCNR/preview" width="1080" height="720"></iframe>
+        <iframe src="https://drive.google.com/file/d/1FkzW2UXELpYt0lIu1nTeAx1bKJqQSCNR/preview" width="1080" height="720"></iframe>
     </div>
   </main>
 );
