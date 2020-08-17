25f56a49a684e3a66c852aa71d9898243095cd04
Jonathan <jonathanvouilloz@gmail.com>
2020-08-17 10:23:27 +0200

    fullscreen

--- src/pages/video/index.js
+++ src/pages/video/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => (
   <main>
     <div>
-        <iframe src="https://drive.google.com/file/d/1FkzW2UXELpYt0lIu1nTeAx1bKJqQSCNR/preview" width="1080" height="720"></iframe>
+        <iframe allow="fullscreen" src="https://drive.google.com/file/d/1FkzW2UXELpYt0lIu1nTeAx1bKJqQSCNR/preview" width="1080" height="720"></iframe>
     </div>
   </main>
 );
