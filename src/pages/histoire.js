e428df3db8949ecf9111e9e42da3c00ff5260633
Jonathan <jonathanvouilloz@gmail.com>
2020-08-23 17:54:23 +0200

    test

--- src/pages/histoire.js
+++ src/pages/histoire.js
@@ -13,14 +13,14 @@ const History = () => (
     <p>Il lui découvrait petit à petit et à chaque minute qui passait, des détails qui ne faisaient que grandir en lui ce sentiment si spécial et chaleureux. Lui n'aimant que très peu les plats ayant du caractère, il arrivait petit à petit à s'y accommoder. Il en venait même à penser qu'il pourrait dérober la recette pour la garder pour lui, mais il ne se retrouvait pas dans ce geste et ne voulait pas ternir l'image qu'il se faisait de lui. C'était tout ce qu'il lui restait. </p>
     <p>Il ne pensait pas possible qu'après tous ce temps, il puisse redécouvrir un plat qui lui fasse déterrer des émotions enfouies au plus profond de son âme. Ces sentiments qui n’arrivent que rarement et qui doivent être minutieusement distillés pour en profiter chacune de ses larmes. C'est pour cette raison qu'il attendit longtemps, impassible, devant ce plat. Une heure et demie s'était écoulé depuis que le serveur était venu lui apporter son assiette. </p>
     <p>Après une attente interminable bien que forte agréable, il se trouvait encore devant ce mets qui n'avait rien perdu de sa superbe. Il avait même gagné en intensité, en mystères et en délicatesse. </p>
-    <p>Le jeune homme n'en pouvait plus, l'attente était trop longue. Il se forçait pour ne pas le dévorer d'une seule traite, car il savait qu'il allait le regretter s’il craquait. </p>
+    <p>Le jeune homme n'en pouvait plus, l'attente était trop longue. Il se forçait pour ne pas le dévorer d'une seule traite, car il savait qu'il allait le regretter s’il craquait. </p><br />
     <p>Il avait appris de ce plat qu'il ne méritait que tendresse et douceur et ne voulait en aucun cas gâcher l'expérience qu'il allait vivre. Il arrivait au terme de son voyage, il le sentait. La tension
-    était de plus en palpable et l’odeur plus intense, mais au fond de lui, il ne voulait pas céder. Céder était synonyme de fin et il ne le souhaitait pour rien au monde tant ce périple était jouissif. </p>
+    était de plus en palpable et l’odeur plus intense, mais au fond de lui, il ne voulait pas céder. Céder était synonyme de fin et il ne le souhaitait pour rien au monde tant ce périple était jouissif. </p><br />
     <p>Le temps était compté pour lui, contrairement au plat qui ne semblait jamais dépérir. Il devait faire un choix avant qu’il ne soit trop tard, mais c’était la première fois de sa vie qu’il faisait face à un choix aussi draconien. Il ne voulait pas plus tarder de peur que le futur choisisse à sa place, il ne voulait pas que l’histoire se renouvelle.  </p>
-    <p>Le jeune homme était insupporté de sa propre décision, il cherchait, en vain et en larmes, d’autres solutions, explorant ses rêves, sans espoir, dans les derniers instants qu’il lui restait. </p>
-    <p>Il était temps. </p>
-    <p>L’ombre qu’il représentait se leva et laissa ce pourquoi il était venu, seul. Il ne put s'y résoudre à y toucher. </p>
-    <p>Au moment de s'en aller, se retournant pour une dernière fois, il aperçut une goutte d’eau s’échapper de l’enveloppe charnelle de l’assiette. Il espéra de tout son cœur que cette goutte ne soit que l’une de celles qui coulèrent sur sa joue. </p>
+    <p>Le jeune homme était insupporté de sa propre décision, il cherchait, en vain et en larmes, d’autres solutions, explorant ses rêves, sans espoir, dans les derniers instants qu’il lui restait. </p><br />
+    <p>Il était temps. </p><br />
+    <p>L’ombre qu’il représentait se leva et laissa ce pourquoi il était venu, seul. Il ne put s'y résoudre à y toucher. </p><br />
+    <p>Au moment de s'en aller, se retournant pour une dernière fois, il aperçut une goutte d’eau s’échapper de l’enveloppe charnelle de l’assiette. Il espéra de tout son cœur que cette goutte ne soit que l’une de celles qui coulèrent sur sa joue. </p><br />
     <br />
 Fin (vraiment ?)
     <br />
