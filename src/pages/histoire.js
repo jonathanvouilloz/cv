e6020b1b9fa4b82c9aac7df0455959bbe12c3e92
Jonathan <jonathanvouilloz@gmail.com>
2020-08-23 17:39:18 +0200

    test

---
+++ src/pages/histoire.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './histoire.css';
+
+const Home = () => (
+  <div class="main">
+    <h2>Des Illusions</h2>
+    <p>
+      C'est l'histoire d'un homme qui adorait manger, après plusieurs mésaventures qui lui avaient fait perdre peu à peu son grain de folie, il ne souhaitait plus que des choses simples dans sa vie, même le simple fait de se nourrir était devenu quelque chose d'éprouvant pour lui. </p>
+    <p>Il avait découvert dans un petit restaurant sympa qu'il fréquentait depuis trois ans un plat qui lui semblait tout bonnement incroyable. Il était composé de tout ce qu’il y avait de plus classique mais la façon dont il était décoré le laissait perplexe. Ce mets d’apparence ordinaire poussa le jeune homme à le découvrir tous ses aspects bien qu'il ne fût pas du genre à se laisser tenter par l’inconnu. </p>
+    <p>Ce plat semblait receler des saveurs encore inexplorées jusqu'ici par le jeune homme, un charme qui lui faisait presque peur, mais que minute après minute lui semblait de plus en plus délicat. </p>
+    <p>Pour une raison qu'il ignorait, il n'arrivait pas à se lancer dans la découverte de ce plat, il restait, contemplatif et pensif, devant ce trésor que la vie lui avait offert avec amour. </p>
+    <p>Il lui découvrait petit à petit et à chaque minute qui passait, des détails qui ne faisaient que grandir en lui ce sentiment si spécial et chaleureux. Lui n'aimant que très peu les plats ayant du caractère, il arrivait petit à petit à s'y accommoder. Il en venait même à penser qu'il pourrait dérober la recette pour la garder pour lui, mais il ne se retrouvait pas dans ce geste et ne voulait pas ternir l'image qu'il se faisait de lui. C'était tout ce qu'il lui restait. </p>
+    <p>Il ne pensait pas possible qu'après tous ce temps, il puisse redécouvrir un plat qui lui fasse déterrer des émotions enfouies au plus profond de son âme. Ces sentiments qui n’arrivent que rarement et qui doivent être minutieusement distillés pour en profiter chacune de ses larmes. C'est pour cette raison qu'il attendit longtemps, impassible, devant ce plat. Une heure et demie s'était écoulé depuis que le serveur était venu lui apporter son assiette. </p>
+    <p>Après une attente interminable bien que forte agréable, il se trouvait encore devant ce mets qui n'avait rien perdu de sa superbe. Il avait même gagné en intensité, en mystères et en délicatesse. </p>
+    <p>Le jeune homme n'en pouvait plus, l'attente était trop longue. Il se forçait pour ne pas le dévorer d'une seule traite, car il savait qu'il allait le regretter s’il craquait. </p>
+    <p>Il avait appris de ce plat qu'il ne méritait que tendresse et douceur et ne voulait en aucun cas gâcher l'expérience qu'il allait vivre. Il arrivait au terme de son voyage, il le sentait. La tension
+    était de plus en palpable et l’odeur plus intense, mais au fond de lui, il ne voulait pas céder. Céder était synonyme de fin et il ne le souhaitait pour rien au monde tant ce périple était jouissif. </p>
+    <p>Le temps était compté pour lui, contrairement au plat qui ne semblait jamais dépérir. Il devait faire un choix avant qu’il ne soit trop tard, mais c’était la première fois de sa vie qu’il faisait face à un choix aussi draconien. Il ne voulait pas plus tarder de peur que le futur choisisse à sa place, il ne voulait pas que l’histoire se renouvelle.  </p>
+    <p>Le jeune homme était insupporté de sa propre décision, il cherchait, en vain et en larmes, d’autres solutions, explorant ses rêves, sans espoir, dans les derniers instants qu’il lui restait. </p>
+    <p>Il était temps. </p>
+    <p>L’ombre qu’il représentait se leva et laissa ce pourquoi il était venu, seul. Il ne put s'y résoudre à y toucher. </p>
+    <p>Au moment de s'en aller, se retournant pour une dernière fois, il aperçut une goutte d’eau s’échapper de l’enveloppe charnelle de l’assiette. Il espéra de tout son cœur que cette goutte ne soit que l’une de celles qui coulèrent sur sa joue. </p>
+    <br />
+Fin (vraiment ?)
+    <br />
+  </div>
+);
+
+export default Home;
